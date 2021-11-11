2c7a69e8218d9fe75e7dad897d1b5a274a8579ca
thomas <tmeyerwebdev@gmail.com>
2021-11-11 23:36:54 +0000

    Refactored, change  product to category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,39 +2,30 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER_KEY'], password: ENV['ADMIN_USER_PASS']
 
   def index
-    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
 
   def new
-    @product = Product.new
+    @category = Category.new
   end
 
   def create
-    @product = Product.new(product_params)
+    @categoryt = Product.new(product_params)
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end
 
-  def destroy
-    @product = Product.find params[:id]
-    @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
-  end
-
+  
   private
 
-  def product_params
-    params.require(:product).permit(
+  def category_params
+    params.require(:category).permit(
       :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
+      
     )
   end
 end
