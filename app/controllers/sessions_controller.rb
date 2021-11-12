d02bff17dc4c89fe0d8d5645b0085b1d21fc2bf0
thomas <tmeyerwebdev@gmail.com>
2021-11-12 23:32:23 +0000

    Removed spelling error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
+    user = User.find_by_email(params[:email])
     
     if user && user.authenticate(params[:password])
-       sessions[:user_id] = user.id
+       session[:user_id] = user.id
        redirect_to '/'
     else
        redirect_to '/login'
