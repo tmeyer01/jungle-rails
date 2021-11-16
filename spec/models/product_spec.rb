4e404aca234d6030abd9445e8b4d5c8464fe4e4a
thomas <tmeyerwebdev@gmail.com>
2021-11-16 02:13:27 +0000

    Add test for product category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Product, type: :model do
   
   describe 'Validations' do
     
-    it 'validate with valid attributes' do
+    it 'validates valid attributes' do
       @category = Category.new(name: "testing")
       @product = @category.products.new(name: 'Slim Jimmy', price: 10, quantity: 10)
       expect(@product).to  be_valid
@@ -32,6 +32,12 @@ RSpec.describe Product, type: :model do
       @product = Product.new(name: 'Slim Jimmy', price: 777, quantity: nil, category: @category)
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'not valid without a category' do
+      @product = Product.new(name: 'Slim Jimmy v4', price: 3, quantity: 600)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
 
